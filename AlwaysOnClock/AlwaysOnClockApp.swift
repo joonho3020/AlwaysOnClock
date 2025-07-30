c0055e33c60fbda70bcbbfa66d31d76691e24303
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-07-30 10:10:20 -0700

    Initial Commit

---
+++ AlwaysOnClock/AlwaysOnClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysOnClockApp.swift
+//  AlwaysOnClock
+//
+//  Created by Joonho Hwangbo on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysOnClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlwaysOnClockTests/AlwaysOnClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysOnClockTests.swift
+//  AlwaysOnClockTests
+//
+//  Created by Joonho Hwangbo on 7/30/25.
+//
+
+import Testing
+@testable import AlwaysOnClock
+
+struct AlwaysOnClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
